0804ae1e46891e088f150265ddb6a50251f82e50
Stef Thoen <stef@baardbaard.nl>
2015-03-27 11:40:31 +0100

    Add licensing.

---
+++ twitterfeed.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: Twitterfeed
+ * Description: Gives you a minimal Twitter feed.
+ * Version: 1.0.0
+ * Author: Stef Thoen
+ * Author URI: http://baardbaard.nl
+ *
+ * Copyright 2015  Stef Thoen  (email : stef@baardbaard.nl)
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+?>
